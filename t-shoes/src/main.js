2da294682543484880c6432950827c145d5f0ee1
DucHD3 <hoangduc22k2002@gmail.com>
2024-12-25 19:58:54 +0700

    m

--- t-shoes/src/main.js
+++ t-shoes/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css"; // Import Element Plus styles
 
-createApp(App).mount('#app')
+// Add the necessary CSS
+export default {
+  devServer: {
+    proxy: "http://localhost:8080",
+  },
+};
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+
+app.mount("#app");
