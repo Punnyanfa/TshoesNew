86fb6e5873c30d961b10d94d83470d0ab7604948
SE151443_TrieuAn <anttse151443@fpt.edu.vn>
2025-02-25 18:49:45 +0700

    register repositories and services

--- Backend/fcsp-webapi/FCSP.WebAPI/Configuration/RepositoryConfig.cs
+++ Backend/fcsp-webapi/FCSP.WebAPI/Configuration/RepositoryConfig.cs
@@ -1,5 +1,7 @@
 ﻿using FCSP.Models.Context;
 using FCSP.Repositories;
+using FCSP.Repositories.Implementations;
+using FCSP.Repositories.Interfaces;
 
 namespace FCSP.WebAPI.Configuration;
 
@@ -12,8 +14,34 @@ internal static class RepositoryConfig
 
     private static void RegisterRepositories(IServiceCollection services)
     {
-        services.AddScoped<IUserRepository, UserRepository>();
-
+        // Register DbContext
         services.AddDbContext<FcspDbContext>();
+        
+        // User repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+        
+        // Post & Comment repositories
+        services.AddScoped<IPostRepository, PostRepository>();
+        services.AddScoped<IPostCommentRepository, PostCommentRepository>();
+        
+        // Texture repositories
+        services.AddScoped<ITextureRepository, TextureRepository>();
+        services.AddScoped<IUserTextureRepository, UserTextureRepository>();
+        
+        // Design repositories
+        services.AddScoped<ICustomShoeDesignRepository, CustomShoeDesignRepository>();
+        services.AddScoped<ICustomShoeDesignTextureRepository, CustomShoeDesignTextureRepository>();
+        services.AddScoped<ITemplateRepository, TemplateRepository>();
+        
+        // Order repositories
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+        
+        // Payment repositories
+        services.AddScoped<IPaymentRepository, PaymentRepository>();
+        services.AddScoped<IPaymentGatewayRepository, PaymentGatewayRepository>();
+        
+        // Shipping repositories
+        services.AddScoped<IShippingInfoRepository, ShippingInfoRepository>();
     }
 }
--- Backend/fcsp-webapi/FCSP.WebAPI/Configuration/ServiceConfig.cs
+++ Backend/fcsp-webapi/FCSP.WebAPI/Configuration/ServiceConfig.cs
@@ -1,6 +1,18 @@
 ﻿using FCSP.Services.Auth;
 using FCSP.Services.Auth.Hash;
 using FCSP.Services.Auth.Token;
+using FCSP.Services.CustomShoeDesignService;
+using FCSP.Services.CustomShoeDesignTextureService;
+using FCSP.Services.OrderDetailService;
+using FCSP.Services.OrderService;
+using FCSP.Services.PaymentGatewayService;
+using FCSP.Services.PaymentService;
+using FCSP.Services.PostCommentService;
+using FCSP.Services.PostService;
+using FCSP.Services.ShippingInfoService;
+using FCSP.Services.TemplateService;
+using FCSP.Services.TextureService;
+using FCSP.Services.UserTextureService;
 using Microsoft.AspNet.Identity;
 
 namespace FCSP.WebAPI.Configuration;
@@ -14,11 +26,35 @@ internal static class ServiceConfig
 
     private static void RegisterServices(IServiceCollection services)
     {
+        // Authentication services
         services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddScoped<IPasswordHashingService, PasswordHashingService>();
         services.AddScoped<ITokenService, JwtService>();
-
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IGoogleAuthService, GoogleAuthService>();
+        
+        // Design services
+        services.AddScoped<ITemplateService, TemplateService>();
+        services.AddScoped<ICustomShoeDesignService, CustomShoeDesignService>();
+        services.AddScoped<ICustomShoeDesignTextureService, CustomShoeDesignTextureService>();
+        
+        // Texture services
+        services.AddScoped<ITextureService, TextureService>();
+        services.AddScoped<IUserTextureService, UserTextureService>();
+        
+        // Order services
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IOrderDetailService, OrderDetailService>();
+        
+        // Payment services
+        services.AddScoped<IPaymentService, PaymentService>();
+        services.AddScoped<IPaymentGatewayService, PaymentGatewayService>();
+        
+        // Post services
+        services.AddScoped<IPostService, PostService>();
+        services.AddScoped<IPostCommentService, PostCommentService>();
+        
+        // Shipping services
+        services.AddScoped<IShippingInfoService, ShippingInfoService>();
     }
 }
