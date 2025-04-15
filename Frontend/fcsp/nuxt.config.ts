015c7488b7e3fa345719d147ea59a5fc80ba681c
Hoang Dinh Duc <144517413+HOANGDUC85@users.noreply.github.com>
2025-04-15 12:52:23 +0700

    Update nuxt.config.ts

--- Frontend/fcsp/nuxt.config.ts
+++ Frontend/fcsp/nuxt.config.ts
@@ -1,40 +1,40 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  build: {
-    transpile: ['swiper', 'element-plus/es']
-  },
-  css: [
-    '~/assets/css/swiper.css',
-    'element-plus/dist/index.css',
-    'bootstrap/dist/css/bootstrap.min.css',
-    'bootstrap-icons/font/bootstrap-icons.css'
-  ],
-  modules: [
-    '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt'
-  ],
-  plugins: [
-    '~/plugins/element-plus.ts',
-    '~/plugins/bootstrap.client.ts'
-  ],
-  app: {
-    head: {
-      script: [
-        {
-          src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
-          defer: true
-        },
-        {
-          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js',
-          defer: true
-        }
-      ]
-    }
-  },
-  vite: {
-    optimizeDeps: {
-      include: ['element-plus', '@element-plus/icons-vue']
-    }
-  }
-})
+// export default defineNuxtConfig({
+//   compatibilityDate: '2024-11-01',
+//   devtools: { enabled: true },
+//   build: {
+//     transpile: ['swiper', 'element-plus/es']
+//   },
+//   css: [
+//     '~/assets/css/swiper.css',
+//     'element-plus/dist/index.css',
+//     'bootstrap/dist/css/bootstrap.min.css',
+//     'bootstrap-icons/font/bootstrap-icons.css'
+//   ],
+//   modules: [
+//     '@pinia/nuxt',
+//     '@pinia-plugin-persistedstate/nuxt'
+//   ],
+//   plugins: [
+//     '~/plugins/element-plus.ts',
+//     '~/plugins/bootstrap.client.ts'
+//   ],
+//   app: {
+//     head: {
+//       script: [
+//         {
+//           src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+//           defer: true
+//         },
+//         {
+//           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js',
+//           defer: true
+//         }
+//       ]
+//     }
+//   },
+//   vite: {
+//     optimizeDeps: {
+//       include: ['element-plus', '@element-plus/icons-vue']
+//     }
+//   }
+// })
