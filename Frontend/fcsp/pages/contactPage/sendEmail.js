59f1d43d4c3ad1a5f03b533039db52f939478909
Hoang Duc <duchdse162086@fpt.edu.vn>
2025-05-30 12:33:32 +0700

    d

--- Frontend/fcsp/pages/contactPage/sendEmail.js
+++ Frontend/fcsp/pages/contactPage/sendEmail.js
@@ -1,6 +1,6 @@
 import { sendEmail } from '../../server/auth/senEmail-service';
 
-export async function sendEmail(userId, subject, body) {
+export async function sendEmailToUser(userId, subject, body) {
   return axios.post(
     "https://fcspwebapi20250527114117.azurewebsites.net/api/Auth/SendEmailToUser",
     {
