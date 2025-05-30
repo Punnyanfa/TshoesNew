0242c8c6db95c5d1de56b60466b660e80900d0f4
Punnyanfa <phamvantrieuthuan12345@gmail.com>
2025-05-30 07:58:19 +0700

    Create ServiceServiceUpdateTests.cs

---
+++ Backend/fcsp-webapi/FCSP.Tests/ServiceServiceUpdateTests.cs
@@ -0,0 +1,32 @@
+﻿using FCSP.Repositories.Interfaces;
+using FCSP.Services.ServiceService;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCSP.Tests
+{
+    public class ServiceServiceUpdateTests
+    {
+        private readonly Mock<IServiceRepository> _serviceRepositoryMock;
+        private readonly Mock<IManufacturerRepository> _manuRepositoryMock;
+        private readonly ServiceService _serviceService;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        public ServiceServiceUpdateTests() 
+        {
+            _serviceRepositoryMock = new Mock<IServiceRepository>();
+            _manuRepositoryMock = new Mock<IManufacturerRepository>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _serviceService = new ServiceService(_serviceRepositoryMock.Object, _manuRepositoryMock.Object, _userRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task ServiceServiceUpdate_IdIsZero()
+        {
+
+        }
+    }
+}
